e7374431e1ced62f6aaba314576913ed76a6bcd2
Bukisoo <p.kidpaddle@gmail.com>
2023-11-14 21:36:30 +0100

    spawner upadte

--- Assets/Script/Spawner.cs
+++ Assets/Script/Spawner.cs
@@ -1,30 +1,52 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Spawner : MonoBehaviour
 {
     public GameObject prefabToSpawn;
     public int numberOfInstances;
     public float spawnDelay = 0.25f;
+    private List<GameObject> spawnedInstances;
 
     void Start()
     {
+        spawnedInstances = new List<GameObject>();
         StartCoroutine(SpawnPrefabs());
     }
 
     void Update()
     {
-    
+        // Check and spawn prefabs if any are missing
+        MaintainPrefabCount();
     }
 
     IEnumerator SpawnPrefabs()
     {
         for (int i = 0; i < numberOfInstances; i++)
         {
-            GameObject instance = Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
-            instance.transform.parent = this.transform;
+            if (spawnedInstances.Count < numberOfInstances)
+            {
+                SpawnPrefab();
+                yield return new WaitForSeconds(spawnDelay);
+            }
+        }
+    }
 
-            yield return new WaitForSeconds(spawnDelay);
+    void SpawnPrefab()
+    {
+        GameObject instance = Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
+        instance.transform.parent = this.transform;
+        spawnedInstances.Add(instance);
+    }
+
+    void MaintainPrefabCount()
+    {
+        spawnedInstances.RemoveAll(item => item == null);
+
+        while (spawnedInstances.Count < numberOfInstances)
+        {
+            SpawnPrefab();
         }
     }
 
