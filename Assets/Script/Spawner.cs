d8c72d90e03c1e121699ea9229c0b6b4ee443b53
Bukisoo <p.kidpaddle@gmail.com>
2023-11-14 22:04:38 +0100

    Revert "ingredient prout"
    This reverts commit 58d1e1af4f7c7b699684dfdd3ae8c825574deca0.

--- Assets/Script/Spawner.cs
+++ Assets/Script/Spawner.cs
@@ -7,7 +7,6 @@ public class Spawner : MonoBehaviour
     public GameObject prefabToSpawn;
     public int numberOfInstances;
     public float spawnDelay = 0.25f;
-    public float positionFuzziness = 0.00001f; // Adjust this value to set the range of randomness
     private List<GameObject> spawnedInstances;
 
     void Start()
@@ -36,12 +35,7 @@ public class Spawner : MonoBehaviour
 
     void SpawnPrefab()
     {
-        Vector3 randomOffset = new Vector3(
-            Random.Range(-positionFuzziness, positionFuzziness),
-            Random.Range(-positionFuzziness, positionFuzziness),
-            Random.Range(-positionFuzziness, positionFuzziness));
-
-        GameObject instance = Instantiate(prefabToSpawn, transform.position + randomOffset, Quaternion.identity);
+        GameObject instance = Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
         instance.transform.parent = this.transform;
         spawnedInstances.Add(instance);
     }
