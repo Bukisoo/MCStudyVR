c4d691fa5be121a31aaf3f99c8eaf3aa1bd8430d
Bukisoo <p.kidpaddle@gmail.com>
2023-11-15 09:11:23 +0100

    prout

--- Assets/Script/StackIngredient.cs
+++ Assets/Script/StackIngredient.cs
@@ -8,24 +8,30 @@ public class StackIngredient : MonoBehaviour
     {
         GameObject other = collision.gameObject;
 
-        // Check if the other object is an ingredient and not already a child of another object
-        if (other.CompareTag("Ingredient") && other.transform.parent == null)
+        // Check if this object is the bottom bun and the other object is an ingredient
+        if (other.CompareTag("Ingredient") && gameObject.CompareTag("BottomBun"))
         {
             // Set this object to be the parent of the collided object
             other.transform.SetParent(transform);
 
-            // Position the ingredient at the attachment point
-            if (attachmentPoint != null)
+            // Calculate the position for the new ingredient
+            Vector3 newPosition = attachmentPoint != null ? attachmentPoint.position : transform.position;
+            if (transform.childCount > 1) // There are already other ingredients attached
             {
-                other.transform.position = attachmentPoint.position;
-                other.transform.rotation = attachmentPoint.rotation;
+                // Adjust the newPosition to be slightly above the highest child
+                float highestPoint = 0f;
+                foreach (Transform child in transform)
+                {
+                    if (child.position.y > highestPoint)
+                    {
+                        highestPoint = child.position.y;
+                    }
+                }
+                newPosition.y = highestPoint + 0.1f; // Adjust this value to set the gap between ingredients
             }
-            else
-            {
-                // Default behavior if no attachment point is specified
-                other.transform.localPosition = Vector3.zero;
-                other.transform.localRotation = Quaternion.identity;
-            }
+
+            other.transform.position = newPosition;
+            other.transform.rotation = Quaternion.identity; // Adjust if necessary
 
             // Optionally, disable physics on the child to prevent further collisions
             Rigidbody rb = other.GetComponent<Rigidbody>();
